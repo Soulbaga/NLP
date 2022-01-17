9d3956bc1d22fcc2f6e3b9f2024060b80f426430
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-17 15:30:08 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
-!pip3 install fake_useragent
+import os
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+
+from google.cloud import language
+from google.cloud.language import types
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+from fake_useragent import UserAgent
+import requests
+import pandas as pd
+import numpy as np
