e55f4c701bc46dab426ce648219892ce2ce0c666
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-19 15:18:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-st.title('NLP Test')
 
+
+$ streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
