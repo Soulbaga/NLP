27f463044c6fe415df270ac02359ce77333ed784
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-19 15:02:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import fake_useragent
 import os
-from google.cloud import language_v1
-from google.cloud.language_v1 import enums
+#from google.cloud import language_v1
+#from google.cloud.language_v1 import enums
 
-from google.cloud import language
-from google.cloud.language import types
+#from google.cloud import language
+#from google.cloud.language import types
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
 from fake_useragent import UserAgent
 import requests
-import pandas as pd
+#import pandas as pd
 import numpy as np
 
 
-from google.colab import files
+from laptop import files
 uploaded = files.upload()
 for fn in uploaded.keys():
   print('User uploaded file "{name}" with length {length} bytes'.format(
