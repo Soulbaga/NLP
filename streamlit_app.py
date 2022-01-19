621ab195f37b7634861c43c260e8caa1e1fe0b60
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-19 14:55:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-run "from google.colab import files
-uploaded = files.upload()
-for fn in uploaded.keys():
-  print('User uploaded file "{name}" with length {length} bytes'.format(
-      name=fn, length=len(uploaded[fn])))"
+import streamlit as st
+import pandas as pd
+import numpy as np
