b95ccda2732d76f73b5f5acbfe16a464db7bed8d
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-24 11:38:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,67 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-st.title('NLP Test')
+# Create a webapp with Streamlit to detect top entities in a uploaded TXT file
+# Use the google.cloud language_v1 API
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+from google.cloud import language_v1
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+
+def sample_analyze_entities(gcs_content_uri):
+    """
+    Analyzing Entities in text file stored in Cloud Storage
+
+    Args:
+      gcs_content_uri Google Cloud Storage URI where the file content is located.
+      e.g. gs://[Your Bucket]/[Path to File]
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # gcs_content_uri = 'gs://cloud-samples-data/language/entity.txt'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"gcs_content_uri": gcs_content_uri, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entities(request={"document": document, "encoding_type": encoding_type})
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+
+
+def main():
+    sample_analyze_entities("gs://cloud-samples-data/language/entity.txt")
+
+
+if __name__ == "__main__":
+    main()
