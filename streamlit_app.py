8d37af2b9d77031d4a436b0e02543b9ff93b1c30
Soulbaga <97893284+Soulbaga@users.noreply.github.com>
2022-01-25 11:11:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,1 @@
-sudo pip install requests
-
-import requests
-
-
-url = "https://www.google.com/search?q=hello+world"
-
-response = requests.get(url)
-
-print(response.text)
+install Phython Library
